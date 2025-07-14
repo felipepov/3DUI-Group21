05a25209f4b989ea07d4de6617bd98afda0aba80
Beliz Pehlivan <belizpehlivan@gmail.com>
2025-07-14 22:04:39 +0300

    Task02-Fire

--- Assets/FireBowlManager.cs
+++ Assets/FireBowlManager.cs
@@ -4,39 +4,19 @@ using UnityEngine;
 
 public class FireBowlManager : MonoBehaviour
 {
-    public ParticleSystem fireEffect;
-    public int woodCount = 0;
-    public int requiredWood = 3;
-
-    public GameObject[] teleportPoints;
+    public GameObject fire;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Wood"))
         {
-            Destroy(other.gameObject); 
-            woodCount++;
-            IncreaseFire();
-
-            if (woodCount >= requiredWood)
-            {
-                ActivateTeleportPoints();
-            }
+            ActivateFire();
+            Destroy(other.gameObject);
         }
     }
 
-    void IncreaseFire()
+    void ActivateFire()
     {
-        var main = fireEffect.main;
-        main.startSize = Mathf.Clamp(main.startSize.constant + 0.5f, 1f, 4f);
-    }
-
-    void ActivateTeleportPoints()
-    {
-        foreach (GameObject tp in teleportPoints)
-        {
-            tp.SetActive(true);
-            tp.GetComponent<ParticleSystem>()?.Play(); 
-        }
+        fire.SetActive(true);
     }
 }
