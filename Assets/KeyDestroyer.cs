0745926a6a13ad8fc7615bedc375a1cc794de972
Ferran Sordé <ferran.sorde01@estudiant.upf.edu>
2025-07-27 11:25:35 +0200

    Key animation and end menu

--- Assets/KeyDestroyer.cs
+++ Assets/KeyDestroyer.cs
@@ -3,16 +3,19 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class KeyPickup : MonoBehaviour
 {
+    private Animator animator;
     private XRGrabInteractable grabInteractable;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         grabInteractable = GetComponent<XRGrabInteractable>();
         grabInteractable.selectEntered.AddListener(OnGrabbed);
     }
 
     void OnGrabbed(SelectEnterEventArgs args)
     {
+        animator.SetTrigger("PickedUp");
         Destroy(gameObject);
     }
 
